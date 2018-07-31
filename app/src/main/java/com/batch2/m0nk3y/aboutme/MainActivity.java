381a2c432595ea66f3543a4835b5fe8a6b601197
minhazRTK <minhaz.rtk@gmail.com>
2018-07-31 23:24:56 +0600

    basic job done!

--- app/src/main/java/com/batch2/m0nk3y/aboutme/MainActivity.java
+++ app/src/main/java/com/batch2/m0nk3y/aboutme/MainActivity.java
@@ -2,6 +2,10 @@ package com.batch2.m0nk3y.aboutme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView img = findViewById(R.id.img);
+        Button btn = findViewById(R.id.btn);
+        final TextView txt = findViewById(R.id.about);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img.setImageResource(R.drawable.pp);
+                String name = "Hello there! \nI'm Minhaz Rahman \n ID:20161012010\n North Western University, Khulna";
+                txt.setText(name);
+            }
+        });
     }
 }
